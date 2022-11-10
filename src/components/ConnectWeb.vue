cc518f8870f43fa5a43bd06e7a566ddbfe9c1bb5
wellensmedia <wellensmedia@gmail.com>
2022-11-10 15:46:31 +0100

    Connect BTN

--- src/components/ConnectWeb.vue
+++ src/components/ConnectWeb.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="user" @click="login=!login">
+  <div class="user login" v-if="!logged" @click="login=true">
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0S96 57.3 96 128s57.3 128 128 128zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
   </div>
-  <div class="bgoverlay" v-if="login" @click="login=false"></div>
+  <div class="user login" v-if="logged">
+    Bonjour Ludovic Wellens
+    <div class="disconnect" @click="logged=false">Me déconnecter</div>
+  </div>
+  <div class="bgoverlay" v-if="login" @click="closeConnect"></div>
   <div class="overlay" v-if="login">
+    <h2>Connexion</h2>
     <label>User</label>
-    <input type="text" />
+    <input type="text" v-model="user" />
     <label>Pass</label>
-    <input type="text" />
+    <input type="password" v-model="pass" />
+    <p class="error" v-if="error">Mauvais utilisateur ou mot de passe</p>
+    <button @click="connect">Connexion</button>
   </div>
 </template>
 
@@ -16,13 +23,38 @@ export default {
   name: 'ConnectWeb',
   data(){
     return {
-      login: false
+      login: false,
+      user: "",
+      pass: "",
+      error: false,
+      logged: false
+    }
+  },
+  methods: {
+    closeConnect(){
+      this.login = false
+      this.pass = ""
+      this.error = false
+    },
+    connect(){
+      if(this.user=="admin" && this.pass=="admin"){
+        this.login = false
+        this.logged = true
+        this.error = false
+        this.pass = ""
+      }else{
+        this.error = true
+        this.pass = ""
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.login{
+  cursor: pointer;
+}
 svg{
   height: 30px;
 }
@@ -47,7 +79,19 @@ svg{
   flex-direction: column;
   background: white;
   justify-content: center;
+  gap: 0.5rem;
   padding: 1rem;
   box-shadow: 0px 0px 10px rgba(0,0,0,0.3);
+}
+input{
+  padding: 0.5rem;
+}
+button{
+  display: block;
+  padding: 0.5rem;
+  cursor: pointer;
+}
+.error{
+  color: red;
 }
 </style>
